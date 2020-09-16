b37ed76f9e5169eda986537b8c7b042a55ca9d18
Francis Ofori <eaabban699@gmail.com>
2020-09-16 18:29:03 -0500

    New Update

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,6 @@
 var krms_config ={						
 	'ApiUrl':"https://restomulti.herokuapp.com/mobileappv2/api",    
 	'AppTitle':"BARGAINGRUB",
-	'ApiKey':'ab026d43d72373ea59773bc8812fdd9f2',	
- 	'LicenseKey': '4e3a60b390a77c3b3d2bba47d7fb8b2b',	
+	'ApiKey' : 'ab026d43d72373ea59773bc8812fdd9f2',	
 	'debug': false
 };
